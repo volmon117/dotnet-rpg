32dd3ee2aa8a04e86ba044c9b3226dee23867a50
Santiago <92620191+volmon117@users.noreply.github.com>
2023-06-15 15:57:09 -0600

    Controller Get and Modules

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,7 @@
+namespace dotnet_rpg.Controllers
+{
+    public class CharacterControler
+    {
+        
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    using System.Text.Json.Serialization;
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
